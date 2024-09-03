20f9f6a31ff9f3c7504742941f112dcf9a4dc0b2
habiba zguaid <zguaidhabiba@gmail.com>
2024-09-03 12:32:19 +0100

    task1

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -26,7 +26,7 @@ def stats() -> str:
     return jsonify(stats)
 
 @app_views.route('/unauthorized', methods=['GET'], strict_slashes=False)
-def unauthorized_error() -> str:
+def unauthorized():
     """ GET /api/v1/unauthorized
     Return:
     - raise a 401 error by using abort - Custom Error Pages
