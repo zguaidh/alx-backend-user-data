185c751a7423fa8232e5d790c6a9a406be781b07
habiba zguaid <zguaidhabiba@gmail.com>
2024-09-03 12:54:21 +0100

    task1

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -20,15 +20,15 @@ def not_found(error) -> str:
     """
     return jsonify({"error": "Not found"}), 404
 
+
 @app.errorhandler(401)
 def unauthorized_error(error) -> str:
     """ unauthorized handler
     """
     return jsonify({"error": "unauthorized"}), 401
 
+
 if __name__ == "__main__":
     host = getenv("API_HOST", "0.0.0.0")
     port = getenv("API_PORT", "5000")
     app.run(host=host, port=port)
-
-
