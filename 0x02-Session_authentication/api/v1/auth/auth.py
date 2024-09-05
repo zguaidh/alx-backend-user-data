f61908273b1535a6896c528fadc9d1af205df6c4
habiba zguaid <zguaidhabiba@gmail.com>
2024-09-05 17:09:39 +0100

    task0

---
+++ 0x02-Session_authentication/api/v1/auth/auth.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+"""
+Module for the class Auth
+"""
+
+from flask import request
+from typing import List, TypeVar
+import re
+
+
+class Auth:
+    """ Auth class definition
+    """
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """Define which routes don't need authentication"""
+        if path is None or excluded_paths is None or not excluded_paths:
+            return True
+        if path in excluded_paths or path + "/" in excluded_paths:
+            return False
+        for excluded_path in excluded_paths:
+            if excluded_path.endswith("*"):
+                if path.startswith(excluded_path[:-1]):
+                    return False
+        return True
+
+    def authorization_header(self, request=None) -> str:
+        """ method that validate all requests to secure the API"""
+        if request is None:
+            return None
+        auth_header = request.headers.get('Authorization')
+        if auth_header is None:
+            return None
+        return auth_header
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """ return None - request will be the Flask request object"""
+        return None
