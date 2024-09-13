045ef8792c53a5fda7eaaa751a4c73ff608cad07
habiba zguaid <zguaidhabiba@gmail.com>
2024-09-13 19:14:22 +0100

    updated valid_login  method

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -57,7 +57,9 @@ class Auth:
             user = self._db.find_user_by(email=email)
         except NoResultFound:
             return False
-        return checkpw(password.encode('utf-8'), user.hash_password)
+        if checkpw(password.encode('utf-8'), user.hash_password):
+            return True
+        return False
 
     def create_session(self, email: str) -> str:
         """create a new session for user
